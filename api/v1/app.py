95d9d8615b251f1068593bf90e59b82773f6964c
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-26 16:15:57 +0400

    Added 404 not found message

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,12 @@ def close_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Return a 404 error"""
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", 5000)
