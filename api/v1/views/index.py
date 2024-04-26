5de1fec990ded9f9a2417bab6a4c3776887397d6
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-26 16:13:30 +0400

    Previous one does not work, but this need to be work because it is correect (i home)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
@@ -16,12 +22,13 @@ def status():
 def stats():
     """Return stats"""
 
-    classes = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users",
-    }
-    return jsonify({key: storage.count(value) for key, value in classes.items()})
+    return jsonify(
+        {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User),
+        }
+    )
