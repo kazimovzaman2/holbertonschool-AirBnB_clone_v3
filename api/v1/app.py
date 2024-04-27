39a7df0a625f0982be24ad2965594ee65edfd586
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-27 12:38:18 +0400

    CORS configuration for backend

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
