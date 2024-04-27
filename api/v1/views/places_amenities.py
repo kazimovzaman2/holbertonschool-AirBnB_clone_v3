a6e0ac14dc9b52d8f238db0c3531e11f814f7d02
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-04-27 16:51:09 +0400

    Update get method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -14,19 +14,23 @@ from models.amenity import Amenity
     methods=["GET"],
     strict_slashes=False,
 )
-def get_place_amenities(place_id):
+def amenity_by_place(place_id):
     """
-    Retrieves the list of amenities by place
+    get all amenities of a place
+    :param place_id: amenity id
+    :return: all amenities
     """
-    amenity_list = []
-    place_obj = storage.get(Place, place_id)
+    fetched_obj = storage.get("Place", str(place_id))
 
-    if place_obj is None:
+    all_amenities = []
+
+    if fetched_obj is None:
         abort(404)
-    for obj in place_obj.amenities:
-        amenity_list.append(obj.to_json())
 
-    return jsonify(amenity_list)
+    for obj in fetched_obj.amenities:
+        all_amenities.append(obj.to_json())
+
+    return jsonify(all_amenities)
 
 
 @app_views.route(
